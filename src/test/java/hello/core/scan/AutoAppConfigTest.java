d0d924fab24662db539658b1becb78ad0f9e0a74
jaewoopk <jaewoopk000@naver.com>
2023-12-18 15:22:16 +0900

    Learn about why should I use about final for 생성자

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AutoAppConfigTest {
-    @Test
-    void basicScan() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
-        MemberService memberService = ac.getBean(MemberService.class);
-        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
-    }
+//    @Test
+//    void basicScan() {
+//        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+//        MemberService memberService = ac.getBean(MemberService.class);
+//        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+//    }
 }
